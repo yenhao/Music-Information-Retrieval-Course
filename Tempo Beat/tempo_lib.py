ad18b3ad1293ffa55adbfab0c2776c7b9df0974c
yenhao <yenhao0218@gmail.com>
2018-06-06 00:25:01 +0800

    implement by librosa

---
+++ Tempo Beat/tempo_lib.py
@@ -0,0 +1,68 @@
+import os
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_dataset(folder = '../datasets/Ballroom/'):
+    train_folder = os.path.join(folder, 'BallroomData')
+    label_folder = os.path.join(folder, 'BallroomAnnotations')
+
+    music_files = {}
+    with open(os.path.join(train_folder, 'allBallroomFiles')) as f:
+        for file_loc in f.readlines():
+            _, genre, filename = file_loc.strip().split('/')
+            if music_files.get(genre) == None:
+                music_files[genre] = {filename: librosa.load(os.path.join(train_folder, file_loc.strip()), sr=None)}
+            else:
+                music_files[genre][filename] = librosa.load(os.path.join(train_folder, file_loc.strip()), sr=None)
+
+    print('File Loaded!')
+    for genre, music in music_files.items():
+        print('\t', genre, len(music))
+
+    return music_files
+    # song_types = [item for item in os.listdir(train_folder) if os.path.isdir(os.path.join(train_folder,item))]
+
+
+def get_fourier_tempogram(y, sr, tempo_second = 8, hop_length = 512, window_length = 2048):
+    D = librosa.stft(y, hop_length=hop_length, window_length=window_length) # spectrogram
+    onset_env = librosa.onset.onset_strength(y, sr=sr, hop_length=hop_length, n_fft=window_length, aggregate=np.median) # novetly curve
+
+    tempo_frame = int(sr/hop_length) * tempo_second  # sub windows size with seconds
+    tempo_hop = int(tempo_frame/8) # while librosa recommend to set as 1
+    tempogram = librosa.stft(onset_env, hop_length=tempo_hop, win_length=tempo_frame, n_fft=1024)
+
+    return tempogram, tempo_hop
+
+
+def bpm_filter(tempogram, tempo_block_size, lower_bpm = 60, upper_bpm=200):
+    lower_bound = int(lower_bpm / tempo_block_size) #
+    upper_bound = int(upper_bpm / tempo_block_size) + 1
+
+    tempogram[:lower_bound, :] = 0
+    tempogram[upper_bound:, :] = 0
+    return tempogram
+
+def filter_tempo(y, sr, tempo_second = 8, hop_length = 512, window_length = 2048):
+
+    tempogram, tempo_hop = get_fourier_tempogram(y, sr, tempo_second = 8, hop_length = 512, window_length = 2048)
+
+    tempo_freq = sr/hop/2 # tempogram's y-axis max value
+    tempo_max_bpm = tempo_freq * 60 # tempogram's max bpm
+    tempo_block_size = tempo_max_bpm / tempogram.shape[0] # get each value of tempogram's size
+
+    filter_tempo_freq = bpm_filter(tempogram, tempo_block_size) # only 60 ~ 200 bpm left
+
+
+if __name__ == "__main__":
+
+    music_files = load_dataset()
+
+    for genre, music in music_files.items():
+        for filename, (y, sr) in music.items():
+            print(genre, filename)
+
+
+
