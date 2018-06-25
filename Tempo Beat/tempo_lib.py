5297b780683475f7987480ac4993d038ff9de706
yenhao <yenhao0218@gmail.com>
2018-06-25 14:44:07 +0800

    final vers

--- Tempo Beat/tempo_lib.py
+++ Tempo Beat/tempo_lib.py
@@ -17,9 +17,9 @@ def load_dataset(folder = '../datasets/Ballroom/'):
             _, genre, filename = file_loc.strip().split('/')
             genre = genre.split('-')[0]
             if music_files.get(genre) == None:
-                music_files[genre] = {filename: {'signal':librosa.load(os.path.join(train_folder, file_loc.strip()), sr=None), 'label':0, 'beats_time':None}}
+                music_files[genre] = {filename: {'signal':librosa.load(os.path.join(train_folder, file_loc.strip()), sr=None), 'label':0, 'beats_time':None, 'down_beats_time':None}}
             else:
-                music_files[genre][filename] = {'signal':librosa.load(os.path.join(train_folder, file_loc.strip()), sr=None), 'label':0, 'beats_time':None}
+                music_files[genre][filename] = {'signal':librosa.load(os.path.join(train_folder, file_loc.strip()), sr=None), 'label':0, 'beats_time':None, 'down_beats_time':None}
 
             #load labels
             with open(os.path.join(label_folder,os.path.join('ballroomGroundTruth', "".join(filename.split('.')[:-1])+'.bpm'))) as l:
@@ -28,7 +28,14 @@ def load_dataset(folder = '../datasets/Ballroom/'):
             #load beats labels
             beat_label_folder = os.path.join(label_folder, os.path.join("BallroomAnnotations", "".join(filename.split('.')[:-1]) + ".beats"))
             with open(beat_label_folder) as f:
-                music_files[genre][filename]['beats_time'] = np.array([float(bt.strip().split()[0]) for bt in f.readlines()])
+                lines = f.readlines()
+                music_files[genre][filename]['beats_time'] = np.array([float(bt.strip().split()[0]) for bt in lines])
+                music_files[genre][filename]['down_beats_time'] = np.array([float(bt.strip().split()[0]) for bt in lines if bt.strip().split()[1] == '1'])
+                # music_files[genre][filename]['down_beats_time'] = []
+                # for bt in f.readlines():
+                #     print(bt.strip().split()[1])
+                #     if bt.strip().split()[1] == '1':
+                #         music_files[genre][filename]['down_beats_time'].append(float(bt.strip().split()[0]))
 
 
     print('File Loaded!')
@@ -50,7 +57,7 @@ def get_fourier_tempogram(y, sr, tempo_second = 8, hop_length = 512, window_leng
     return tempogram, tempo_hop
 
 
-def bpm_filter(tempogram, tempo_block_size, lower_bpm = 60, upper_bpm=220):
+def bpm_filter(tempogram, tempo_block_size, lower_bpm = 80, upper_bpm=210):
     lower_bound = int(lower_bpm / tempo_block_size)
     upper_bound = int(upper_bpm / tempo_block_size) + 1
 
@@ -93,7 +100,7 @@ def get_tempo(y, sr, tempo_gap_rate = 0.25,tempo_second = 8, hop_length = 512, w
         acf_scale = np.array(range(acf_tempogram.shape[0])) * lag_block_size # all possible lag for this tempogram [box_size, box_size*2, box_size*3 ... box_size *400]
         acf_bpm = 60 / (acf_scale+1e-16) # convert lag to bpm
 
-        possible_idx = (acf_bpm <= 220) & (acf_bpm >= 60) # index for bpm 60 ~ 240
+        possible_idx = (acf_bpm <= 210) & (acf_bpm >= 80) # index for bpm 60 ~ 240
         strength_order = np.mean(acf_tempogram, axis=1)[possible_idx].argsort()[::-1] # bpm tempogram order by strength in 60~200
         ordered_freq = acf_bpm[possible_idx][strength_order] # ordered bpm value in 60~200
 
@@ -143,6 +150,33 @@ def get_F1_score(label, pred):
     precision = get_Precision(label, pred)
     recall = get_Recall(label, pred)
     return 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+
+## for downbeats
+def get_possible_frame(beats_strong_list, gap=4):
+    skip = gap
+    variation = gap
+
+    frame_candidate = []
+    for i in range(gap):  # different start point
+        gap_bar = slide_window(beats_strong_list[i:], gap)
+        num_1 = 0
+        for bar in gap_bar:
+            # count strongest in first
+            bar = np.array(bar)
+            if np.argsort(bar)[0] == gap - 1:  # biggest one is 1
+                num_1 += 1  # count on 1
+        frame_candidate.append(num_1 / len(gap_bar))  # average 1 in first placed
+    return (gap, frame_candidate)
+
+
+def slide_window(strong_list, skip):
+    candidate = strong_list[:skip]
+    c_len = len(candidate)
+    if c_len < skip:  # to the end
+        candidate += [0] * (skip - c_len)
+        return [candidate]
+    return [candidate] + slide_window(strong_list[skip:], skip)
+
 
 if __name__ == "__main__":
 
@@ -194,6 +228,51 @@ if __name__ == "__main__":
             print(beats_F1_score)
 
 
+            # downbeats
+            hop_length = 512
+            onset_env = librosa.onset.onset_strength(y, sr=sr, hop_length=hop_length, n_fft=1024)
+            onset_diff = np.append(np.zeros(1), np.diff(onset_env))  # get the difference of onset
+            onset_diff[onset_diff < 0] = 0  # ReLU
+
+            # take strong beats time
+            onset_diff_time = []
+            for i, strong in enumerate(onset_diff):
+                if strong > 0:
+                    onset_diff_time.append((i / (sr / hop_length), strong))
+
+            # get strong degree of each beats time
+            max_len = len(onset_diff_time)
+            strong_weak_beats_list = []
+            for bt in beats_time:
+                for i in range(max_len):
+                    time, strong = onset_diff_time[i]
+                    if time <= bt:  # small than bt
+                        if i + 1 < max_len:
+                            if onset_diff_time[i + 1][0] > bt:  # next bigger than bt
+                                # take value as strong value
+                                strong_weak_beats_list.append(strong)
+                                break  # search for next bt's strong value
+
+            four_bar = get_possible_frame(strong_weak_beats_list, 4) # 4_bar's candidate
+            three_bar = get_possible_frame(strong_weak_beats_list, 3) # 3_bar's candidate
+
+            if max(four_bar[1]) > max(three_bar[1]):  # is 4
+                down_beats_idx = (4, four_bar[1].index(max(four_bar[1])))
+            else:  # is 3
+                down_beats_idx = (3, three_bar[1].index(max(three_bar[1])))
+
+            # down_beats_idx = skip, start_idx
+
+            downbeats_sec = np.array([i for i in beats_time[down_beats_idx[1]::down_beats_idx[0]]])
+
+            print(downbeats_sec)
+            print(sig_label['down_beats_time'])
+            downbeats_F1_score = get_F1_score(sig_label['down_beats_time'], downbeats_sec)
+            downbeats_precision = get_Precision(sig_label['down_beats_time'], downbeats_sec)
+            downbeats_recall = get_Recall(sig_label['down_beats_time'], downbeats_sec)
+            print(downbeats_F1_score)
+            print(downbeats_precision)
+            print(downbeats_recall)
 
             # save to pandas
             p_score_res_pd = p_score_res_pd.append(pd.DataFrame({'genre':[genre], 'music':[filename], 'label' :[label],
@@ -215,9 +294,10 @@ if __name__ == "__main__":
                                     'acf_saliency_p_score_mul3':[acf_p_score_mut_3[0]], 'acf_alotc_p_score_mul3':[acf_p_score_mut_3[1]],
                                     'fourier_saliency_p_score_mul4':[fourier_p_score_mut_4[0]], 'fourier_alotc_p_score_mul4':[fourier_p_score_mut_4[1]],
                                     'acf_saliency_p_score_mul4':[acf_p_score_mut_4[0]], 'acf_alotc_p_score_mul4':[acf_p_score_mut_4[1]],
-                                    'beats_F1_score':beats_F1_score, 'beats_precision':precision, 'beats_recall':recall}))
+                                    'beats_F1_score':beats_F1_score, 'beats_precision':precision, 'beats_recall':recall,
+                                    'downbeats_F1_score': downbeats_F1_score, 'downbeats_precision': downbeats_precision, 'downbeats_recall': downbeats_recall}))
 
-    p_score_res_pd.to_pickle('ballroom_res_pd_60_220_median.pkl')
+    p_score_res_pd.to_pickle('ballroom_res_pd_80_210_median.pkl')
 
 
 
